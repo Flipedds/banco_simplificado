9fd1fb4dcefcc09c10525e6897cd21d3f9e70eaf
Filipe André <flipeandre12@gmail.com>
2024-04-24 14:25:05 -0300

    :tada: commit inicial

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle('Banco Simplificado API')
+    .setDescription('descrição do banco simplificado')
+    .setVersion('1.0')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  await app.listen(3000);
+}
+bootstrap();
